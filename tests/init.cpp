d98d210b250ea92f30d5481e26a530fd146987fc
yar-yasenkov <yar-yasenkov@yandex.ru>
2016-11-06 15:27:18 +0300

    Create init.cpp

---
+++ tests/init.cpp
@@ -0,0 +1,34 @@
+#include <sharedptr.cpp>
+#include <catch.hpp>
+#include <iostream>
+
+SCENARIO("create1", "[CREATE1]")
+{
+  shared_ptr<int> sp;
+  REQUIRE(!sp.get());
+}
+
+SCENARIO("create2", "[CREATE2]")
+{
+  shared_ptr<int> sp(new int(10));
+  REQUIRE(*sp==10);
+  REQUIRE(sp.use_count()==1);
+}
+
+SCENARIO("copy", "[COPY]")
+{
+  shared_ptr<int> sp1(new int(10));
+  shared_ptr<int> sp2=sp1;
+  REQUIRE(*sp2==10);
+  REQUIRE(sp2.use_count()==2);
+}
+
+SCENARIO("assign ", "[prisv1]"){
+  shared_ptr<int> sp1(new int(10));
+  shared_ptr<int> sp2; 
+  sp2=sp1;
+  REQUIRE(*sp2==10);
+  REQUIRE(sp2.countref()==2);
+}
+
+  
