538e62774c1bc5a8aec93ec6204c631924e7c8de
yar-yasenkov <yar-yasenkov@yandex.ru>
2016-11-06 15:38:42 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -4,29 +4,29 @@
 
 SCENARIO("create1", "[CREATE1]")
 {
-  shared_ptr<int> sp;
+  Shared_ptr<int> sp;
   REQUIRE(!sp.get());
 }
 
 SCENARIO("create2", "[CREATE2]")
 {
-  shared_ptr<int> sp(new int(10));
+  Shared_ptr<int> sp(new int(10));
   REQUIRE(*sp==10);
   REQUIRE(sp.use_count()==1);
 }
 
 SCENARIO("copy", "[COPY]")
 {
-  shared_ptr<int> sp1(new int(10));
-  shared_ptr<int> sp2=sp1;
+  Shared_ptr<int> sp1(new int(10));
+  Shared_ptr<int> sp2=sp1;
   REQUIRE(*sp2==10);
   REQUIRE(sp2.use_count()==2);
 }
 
 SCENARIO("assign ", "[ASSIGN]")
 {
-  shared_ptr<int> sp1(new int(10));
-  shared_ptr<int> sp2; 
+  Shared_ptr<int> sp1(new int(10));
+  Shared_ptr<int> sp2; 
   sp2=sp1;
   REQUIRE(*sp2==10);
   REQUIRE(sp2.use_count()==2);
@@ -34,9 +34,9 @@ SCENARIO("assign ", "[ASSIGN]")
 
 SCENARIO("use_count", "[COUNT]")
 {
-   shared_ptr<int> sp1(new int(10));
-   shared_ptr<int> sp2=sp1;
-   shared_ptr<int> sp3;
+   Shared_ptr<int> sp1(new int(10));
+   Shared_ptr<int> sp2=sp1;
+   Shared_ptr<int> sp3;
    sp3=sp1;
    REQUIRE(sp1.use_count()==3);
 }
